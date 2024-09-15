95933c826d1200cb27062be5edd15d5e5fa96e91
Gavyn Ezell <ezellgavyn@gmail.com>
2024-09-14 23:03:24 -0700

    deleted old stuff

--- shaders/cloud/longcloud.frag.js
+++
@@ -1,9 +0,0 @@
-export default /* glsl */`
-uniform sampler2D longcloudTexture;
-in vec2 texCoord;
-
-void main() {
-    vec4 color = texture2D( longcloudTexture, texCoord);
-    color.a *= 0.4;
-    gl_FragColor = color;
-}`;
--- shaders/distantwater/distantwater.vert.js
+++
@@ -1,7 +0,0 @@
-export default /* glsl */`
-out vec2 texCoord;
-
-void main() {
-	texCoord = vec2(uv.x, uv.y);
-	gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4( position, 1.0 );
-}`;
